3863ce8b8a1df7abcbbcaefa03795ab7a6fdfd98
Milan Aryal <considermilan@gmail.com>
2015-07-08 15:23:54 +0545

    Update layout

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -3,7 +3,7 @@
  * Copyright 2015 Milan Aryal
  * Licensed under MIT (https://github.com/MilanAryal/milanaryal.github.io/blob/master/LICENSE)
  *
- * Date: 2015-07-02T18:38:39+05:45
+ * Date: 2015-07-08T15:20:25+05:45
  */
 
 /*!
@@ -11773,86 +11773,82 @@ var anchors = new AnchorJS();
 !function ($) {
   'use strict';
 
-  $(function () {
+  // Navigation Scripts to Show Header on Scroll-Up
+  // ===============================================
 
-    // Tooltip init
-    // ============
+  var MQL = 1170;
 
-    $('[data-toggle="tooltip"]').tooltip()
-
-
-    // Responsive stuffs
-    // =================
-
-    // make all images responsive
-    $('img').addClass('img-responsive');
-
-    // responsive table
-    $('table').wrap('<div class="table-responsive"></div>');
-    $('table').addClass('table table-hover');
-
-    // responsive embed video
-    $('iframe[src*="youtube.com"]').wrap('<div class="embed-responsive embed-responsive-16by9"></div>');
-    $('iframe[src*="youtube.com"]').addClass('embed-responsive-item');
-
-    $('iframe[src*="vimeo.com"]').wrap('<div class="embed-responsive embed-responsive-16by9"></div>');
-    $('iframe[src*="vimeo.com"]').addClass('embed-responsive-item');
-
-    $('iframe[src*="slideshare.net"]').wrap('<div class="embed-responsive embed-responsive-16by9"></div>');
-    $('iframe[src*="slideshare.net"]').addClass('embed-responsive-item');
-
-
-    // Navigation Scripts to Show Header on Scroll-Up
-    // ===============================================
-
-    var MQL = 1170;
-
-    // primary navigation slide-in effect
-    if ($(window).width() > MQL) {
-      var headerHeight = $('.header-navbar').height();
-      $(window).on('scroll', {
-        previousTop: 0
-      },
-      function () {
-        var currentTop = $(window).scrollTop();
-        // check if user is scrolling up
-        if (currentTop < this.previousTop) {
-          // if scrolling up...
-          if (currentTop > 0 && $('.header-navbar').hasClass('is-fixed')) {
-            $('.header-navbar').addClass('is-visible');
-          } else {
-            $('.header-navbar').removeClass('is-visible is-fixed');
-          }
+  // primary navigation slide-in effect
+  if ($(window).width() > MQL) {
+    var headerHeight = $('.header-navbar').height();
+    $(window).on('scroll', {
+      previousTop: 0
+    },
+    function () {
+      var currentTop = $(window).scrollTop();
+      // check if user is scrolling up
+      if (currentTop < this.previousTop) {
+        // if scrolling up...
+        if (currentTop > 0 && $('.header-navbar').hasClass('is-fixed')) {
+          $('.header-navbar').addClass('is-visible');
         } else {
-          // if scrolling down...
-          $('.header-navbar').removeClass('is-visible');
-          if (currentTop > headerHeight && !$('.header-navbar').hasClass('is-fixed')) $('.header-navbar').addClass('is-fixed');
+          $('.header-navbar').removeClass('is-visible is-fixed');
         }
-        this.previousTop = currentTop;
-      });
-    }
+      } else {
+        // if scrolling down...
+        $('.header-navbar').removeClass('is-visible');
+        if (currentTop > headerHeight && !$('.header-navbar').hasClass('is-fixed')) $('.header-navbar').addClass('is-fixed');
+      }
+      this.previousTop = currentTop;
+    });
+  }
 
 
-    // Social share - Helps to pop-up a window on click
-    // =================================================
+  // Tooltip init
+  // ============
 
-    $('.social-share').on('click', function () {
-      var width  = 670; // last update: 575
-      var height = 420; // last update: 400
-      var top    = ($(window).height() - height) / 2;
-      var left   = ($(window).width()  - width)  / 2;
-      var url    = this.href;
-      var opts   = 'status=1' +
-                   ',width='  + width  +
-                   ',height=' + height +
-                   ',top='    + top    +
-                   ',left='   + left;
+  $('[data-toggle="tooltip"]').tooltip()
 
-      window.open(url, '', opts);
 
-      return false;
-    })
+  // Responsive stuffs
+  // =================
 
+  // make all images responsive
+  $('img').addClass('img-responsive');
+
+  // responsive table
+  $('table').wrap('<div class="table-responsive"></div>');
+  $('table').addClass('table table-hover');
+
+  // responsive embed video
+  $('iframe[src*="youtube.com"]').wrap('<div class="embed-responsive embed-responsive-16by9"></div>');
+  $('iframe[src*="youtube.com"]').addClass('embed-responsive-item');
+
+  $('iframe[src*="vimeo.com"]').wrap('<div class="embed-responsive embed-responsive-16by9"></div>');
+  $('iframe[src*="vimeo.com"]').addClass('embed-responsive-item');
+
+  $('iframe[src*="slideshare.net"]').wrap('<div class="embed-responsive embed-responsive-16by9"></div>');
+  $('iframe[src*="slideshare.net"]').addClass('embed-responsive-item');
+
+
+  // Social share - Helps to pop-up a window on click
+  // =================================================
+
+  $('.social-share').on('click', function () {
+    var width  = 670; // last update: 575
+    var height = 420; // last update: 400
+    var top    = ($(window).height() - height) / 2;
+    var left   = ($(window).width()  - width)  / 2;
+    var url    = this.href;
+    var opts   = 'status=1' +
+                 ',width='  + width  +
+                 ',height=' + height +
+                 ',top='    + top    +
+                 ',left='   + left;
+
+    window.open(url, '', opts);
+
+    return false;
   })
 
 }(jQuery);
