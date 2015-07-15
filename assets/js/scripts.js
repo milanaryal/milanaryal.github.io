33442b665d816cceaee4a9d9bb4f60466465e07d
Milan Aryal <considermilan@gmail.com>
2015-07-15 12:10:15 +0545

    Update .lead style

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -3,7 +3,7 @@
  * Copyright 2015 Milan Aryal
  * Licensed under MIT (https://github.com/MilanAryal/milanaryal.github.io/blob/master/LICENSE)
  *
- * Date: 2015-07-13T07:17:05+05:45
+ * Date: 2015-07-15T12:07:04+05:45
  */
 
 /*!
