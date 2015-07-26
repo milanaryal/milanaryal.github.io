7749e93787b7cd760f7e44f8adb13084eb4242d5
Milan Aryal <considermilan@gmail.com>
2015-07-26 18:38:29 +0545

    Update typo style

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -3,7 +3,7 @@
  * Copyright 2015 Milan Aryal
  * Licensed under MIT (https://github.com/MilanAryal/milanaryal.github.io/blob/master/LICENSE)
  *
- * Date: 2015-07-25T22:49:14+05:45
+ * Date: 2015-07-26T18:34:14+05:45
  */
 
 /*!
