ea2887cacdf5ac4c2de37b9d987b1bd68c1b773f
Milan Aryal <considermilan@gmail.com>
2015-08-10 21:04:20 +0545

    Update layout markups and styles

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -3,7 +3,7 @@
  * Copyright 2015 Milan Aryal
  * Licensed under MIT (https://github.com/MilanAryal/milanaryal.github.io/blob/master/LICENSE)
  *
- * Date: 2015-08-09T17:08:12+05:45
+ * Date: 2015-08-10T20:56:32+05:45
  */
 
 /*!
