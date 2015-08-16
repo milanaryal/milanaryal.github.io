8901203a48a73c85a669f5538fe5bb3f9dddf5fa
Milan Aryal <considermilan@gmail.com>
2015-08-16 16:48:44 +0545

    Not using elevator

--- src/js/scripts.js
+++ src/js/scripts.js
@@ -88,40 +88,6 @@
   });
 
 
-  // Elevator - Scroll back to top utility
-  // ======================================
-
-  // append necessary class
-  // should have already contain wrapper on a page.
-  // <div class="elevator-wrapper"></div>
-  $('.elevator-wrapper').append('<div class="elevator"><i class="fa fa-chevron-up" aria-hidden="true"></i></div>');
-
-  // browser window scroll (in pixels) after which the "elevator button" is shown
-  var offset_header = 500,
-    // browser window scroll (in pixels) after which the "elevator button" is hidden
-    offset_footer = $('.info-footer').offset().top - $(window).height() - 50,
-    // duration of the top scrolling animation (in ms)
-    scroll_top_duration = 700,
-    // grab the "back to top" link
-    $back_to_top = $('.elevator');
-
-  // hide or show the "back to top" link
-  $(window).scroll(function () {
-    var $scroll_top = $(this).scrollTop();
-
-    ($scroll_top > offset_header && $scroll_top < offset_footer) ? $back_to_top.addClass('elevator-is-visible') : $back_to_top.removeClass('elevator-is-visible');
-  });
-
-  // smooth scroll to top
-  $back_to_top.on('click', function (event) {
-    event.preventDefault();
-    $('body,html').animate({
-      scrollTop: 0
-      }, scroll_top_duration
-    );
-  });
-
-
 }(jQuery);
 
 
