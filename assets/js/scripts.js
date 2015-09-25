034cffbc63472a7bd88c38b43c4c7ca31fa0434c
Milan Aryal <considermilan@gmail.com>
2015-09-25 20:11:33 +0545

    Fix bugs

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -3,7 +3,7 @@
  * Copyright 2015 Milan Aryal
  * Licensed under MIT (https://github.com/MilanAryal/milanaryal.github.io/blob/master/LICENSE)
  *
- * Date: 2015-09-25T19:34:11+05:45
+ * Date: 2015-09-25T20:10:37+05:45
  */
 
 /*!
