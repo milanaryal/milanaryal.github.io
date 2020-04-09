113435d3d2ef3e78199c551316c583d096acfcf1
Milan Aryal <considermilan@gmail.com>
2020-04-09 12:49:19 +0545

    update dependencies

--- gulpfile.js
+++ gulpfile.js
@@ -15,13 +15,15 @@ const header = require('gulp-header');
 const pkg = require('./package.json');
 const eslint = require('gulp-eslint');
 const concat = require('gulp-concat');
-const uglify = require('gulp-uglify');
+const uglify = require('gulp-uglify-es').default;
 const scssLint = require('gulp-scss-lint');
-const scss = require('gulp-sass');
-scss.compiler = require('node-sass');
-const prefix = require('gulp-autoprefixer');
-const minify = require('gulp-cssnano');
-const svgSprite = require('gulp-svgstore');
+const sass = require('gulp-sass');
+sass.compiler = require('node-sass');
+const postcss = require('gulp-postcss');
+const autoprefixer = require('autoprefixer');
+const cssnano = require('cssnano');
+const discardComments = require('postcss-discard-comments');
+const svgstore = require('gulp-svgstore');
 const svgmin = require('gulp-svgmin');
 const browserSync = require('browser-sync').create();
 const cp = require('child_process');
@@ -65,16 +67,21 @@ var banner = ['/*!',
   ' */',
   ''].join('\n');
 
+
+/**
+ * Cleaning tasks
+ */
+
 // Remove pre-existing content from the folders
 function clean () {
   return del(['assets/js', 'assets/css', 'assets/fonts', 'assets/svg']);
 }
 
-function cleanScripts () {
+function cleanJS () {
   return del(['assets/js']);
 }
 
-function cleanStyles () {
+function cleanCSS () {
   return del(['assets/css']);
 }
 
@@ -82,54 +89,64 @@ function cleanFonts () {
   return del(['assets/fonts']);
 }
 
-function cleanIcons () {
+function cleanSVG () {
   return del(['assets/svg']);
 }
 
+
+/**
+ * Testing tasks
+ */
+
 // Test scripts
-function testScripts () {
+function testJS () {
   return src(['src/js/**/*.js', '!src/js/**/jquery.js', '!src/js/bootstrap/**'])
     .pipe(eslint('.eslintrc.json'))
     .pipe(eslint.format());
 }
 
 // Test styles
-function testStyles () {
-  return src(['src/scss/**/*.scss', '!src/scss/bootstrap/**', '!src/scss/font-awesome/**'])
+function testSCSS () {
+  return src(['src/scss/**/*.scss', '!src/scss/bootstrap/**'])
     .pipe(scssLint({ 'config': '.scss-lint.yml' }));
 }
 
+
+/**
+ * Compiling & bundling tasks
+ */
+
 // Concatenate and minify scripts
-function buildScripts () {
+function js () {
   return src(SCRIPTS_SRC)
     .pipe(concat('scripts.js'))
     .pipe(header(banner, { pkg : pkg }))
     .pipe(dest('assets/js'))
     .pipe(rename({ suffix: '.min' }))
-    .pipe(uglify())
+    .pipe(uglify({ output: { comments: false } }))
     .pipe(header(banner, { pkg : pkg }))
     .pipe(dest('assets/js'));
 }
 
 // Process and minify styles
-function buildStyles () {
+function css () {
   return src('src/scss/styles.scss')
     .pipe(header(banner, { pkg : pkg }))
-    .pipe(scss({ precision: 6, outputStyle: 'expanded' }))
-    .pipe(prefix())
+    .pipe(sass.sync({ precision: 6, outputStyle: 'expanded' }).on('error', sass.logError))
+    .pipe(postcss([ autoprefixer() ]))
     .pipe(dest('assets/css'))
     .pipe(rename({ suffix: '.min' }))
-    .pipe(minify({ discardComments: { removeAll: true } }))
+    .pipe(postcss([ cssnano(), discardComments({ removeAll: true }) ]))
     .pipe(header(banner, { pkg : pkg }))
     .pipe(dest('assets/css'));
 }
 
 // Process and minify SVG icons
-function buildIcons () {
+function svg () {
   return src('src/svg/**/*.svg', { base: 'src/sprite' })
     .pipe(svgmin())
-    .pipe(rename({prefix: 'icon-'}))
-    .pipe(svgSprite({ inlineSvg: true }))
+    .pipe(rename({ prefix: 'icon-' }))
+    .pipe(svgstore({ inlineSvg: true }))
     .pipe(dest('assets/svg'));
 }
 
@@ -140,20 +157,35 @@ function copyFonts () {
     .pipe(dest('assets/fonts'));
 }
 
-// Remove pre-existing Jekyll build site content
-function cleanJekyll () {
+
+/**
+ * Jekyll tasks
+ */
+
+// Remove script files in Jekyll site
+function jekyllCleanJS () {
+  return del(['./_site/assets/js']);
+}
+
+// Remove style files in Jekyll site
+function jekyllCleanCSS () {
+  return del(['./_site/assets/css']);
+}
+
+// Remove existing Jekyll build site contents
+function jekyllCleanSite () {
   return del(['./_site']);
 }
 
 // Build the Jekyll site
-function buildJekyll (done) {
+function jekyllBuild (done) {
   browserSync.notify('Compiling Jekyll, please wait!');
   return cp.spawn('npm', ['run', 'jekyll-build'], { stdio: 'inherit' })
     .on('close', done);
 }
 
 // Wait for Jekyll build, then launch the server
-function syncJekyll () {
+function jekyllServe () {
   browserSync({
     port: 4000,
     ui: {
@@ -165,49 +197,63 @@ function syncJekyll () {
   });
 }
 
-// Rebuild scripts on Jekyll site and do page reload
-function reBuildScripts () {
+// Rebuild JS on Jekyll site and do page reload
+function jekyllJS () {
+    // JS
   return src(SCRIPTS_SRC)
     .pipe(concat('scripts.js'))
     .pipe(header(banner, { pkg : pkg }))
     .pipe(dest('./_site/assets/js'))
     .pipe(rename({ suffix: '.min' }))
-    .pipe(uglify())
+    .pipe(uglify({ output: { comments: false } }))
     .pipe(header(banner, { pkg : pkg }))
     .pipe(dest('./_site/assets/js'))
+    // Browser reload
     .pipe(browserSync.reload({ stream: true }));
 }
 
-// Rebuild styles on Jekyll site and do page reload
-function reBuildStyles () {
+// Rebuild CSS on Jekyll site and do page reload
+function jekyllCSS () {
+  // CSS
   return src('src/scss/styles.scss')
-    .pipe(header(banner, { pkg : pkg }))
-    .pipe(scss({ precision: 6, outputStyle: 'expanded' }))
-    .pipe(prefix())
-    .pipe(dest('./_site/assets/css'))
-    .pipe(rename({ suffix: '.min' }))
-    .pipe(minify({ discardComments: { removeAll: true } }))
-    .pipe(header(banner, { pkg : pkg }))
-    .pipe(dest('./_site/assets/css'))
-    .pipe(browserSync.reload({ stream: true }));
+  .pipe(header(banner, { pkg : pkg }))
+  .pipe(sass.sync({ precision: 6, outputStyle: 'expanded' }).on('error', sass.logError))
+  .pipe(postcss([ autoprefixer() ]))
+  .pipe(dest('./_site/assets/css'))
+  .pipe(rename({ suffix: '.min' }))
+  .pipe(postcss([ cssnano(), discardComments({ removeAll: true }) ]))
+  .pipe(header(banner, { pkg : pkg }))
+  .pipe(dest('./_site/assets/css'))
+  // Browser reload
+  .pipe(browserSync.reload({ stream: true }));
 }
+
+
+/**
+ * Watching task for changes in src files
+ */
 
 // Watch changes
 function watchFiles () {
   // Watch .js files
-  watch('src/js/**/*.js', series(cleanStyles, reBuildStyles));
+  watch('src/js/**/*.js', series(jekyllCleanJS, jekyllJS));
   // Watch .scss files
-  watch('src/scss/**/*.scss', series(cleanScripts, reBuildScripts));
+  watch('src/scss/**/*.scss', series(jekyllCleanCSS, jekyllCSS));
   // Watch Jekyll uncompiled files and do page reload
-  watch(JEKYLL_SRC, series(cleanJekyll, buildJekyll)).on('change', browserSync.reload);
+  watch(JEKYLL_SRC, series(jekyllCleanSite, jekyllBuild)).on('change', browserSync.reload);
 }
 
-// Export tasks
-var build = series(clean, parallel(buildScripts, buildStyles, buildIcons, copyFonts));
-var buildJekyll = series (cleanJekyll, buildJekyll);
-var serveJekyll = series(buildJekyll, syncJekyll);
 
+/**
+ * Export tasks
+ */
+
+var build = series(clean, parallel(js, css, svg, copyFonts));
+var jekyllBuild = series (jekyllCleanSite, jekyllBuild);
+var jekyllServe = series(jekyllBuild, jekyllServe);
+
+// Public working tasks
 exports.default = build;
-exports.buildJekyll = series(build, buildJekyll);
-exports.serveJekyll = serveJekyll;
-exports.watch = series(serveJekyll, watchFiles);
+exports.jekyllBuild = series(build, jekyllBuild);
+exports.jekyllServe = jekyllServe;
+exports.watch = series(jekyllServe, watchFiles);
