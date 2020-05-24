56d589424096952af5f1fa4168e6f05380b18cf3
Milan Aryal <considermilan@gmail.com>
2020-05-24 15:02:06 +0545

    minor improvements
    - update browser suport list
    - changed back to top icon
    - smooth scrool for header anchor links
    - heartbeat animation for heart icon

--- assets/js/bundle.js
+++ assets/js/bundle.js
@@ -14082,6 +14082,16 @@ return Popper;
 
     $(postHeader).wrapInner('<span class="post-content-title"></span>');
 
+    // smooth scroll header links
+    document.querySelectorAll('a.anchor').forEach(function (anchor) {
+      anchor.addEventListener('click', function (e) {
+        e.preventDefault();
+        document.querySelector(this.getAttribute('href')).scrollIntoView({
+          behavior: 'smooth'
+        });
+      });
+    });
+
 
     /**
      * ------------------------------------------------------------------------
@@ -14101,8 +14111,8 @@ return Popper;
      * ------------------------------------------------------------------------
      */
 
-    var iconChevronUp = '<svg class="icon icon-chevron-up" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512"><path d="M240.971 130.524l194.343 194.343c9.373 9.373 9.373 24.569 0 33.941l-22.667 22.667c-9.357 9.357-24.522 9.375-33.901.04L224 227.495 69.255 381.516c-9.379 9.335-24.544 9.317-33.901-.04l-22.667-22.667c-9.373-9.373-9.373-24.569 0-33.941L207.03 130.525c9.372-9.373 24.568-9.373 33.941-.001z"/></svg>';
-    var divElevator = '<div class="elevator" aria-hidden="true">' + iconChevronUp + '</div>';
+    var iconArrowUp = '<svg class="icon icon-arrow-up" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512"><path d="M34.9 289.5l-22.2-22.2c-9.4-9.4-9.4-24.6 0-33.9L207 39c9.4-9.4 24.6-9.4 33.9 0l194.3 194.3c9.4 9.4 9.4 24.6 0 33.9L413 289.4c-9.5 9.5-25 9.3-34.3-.4L264 168.6V456c0 13.3-10.7 24-24 24h-32c-13.3 0-24-10.7-24-24V168.6L69.2 289.1c-9.3 9.8-24.8 10-34.3.4z"/></svg>';
+    var divElevator = '<div class="elevator" aria-hidden="true">' + iconArrowUp + '</div>';
     var $elevatorWrapper = $('.elevator-wrapper');
 
     $elevatorWrapper.removeAttr('hidden').append(divElevator);
