b3e4a73be068a97614f83a6461366c06f6e4aefa
Milan Aryal <considermilan@gmail.com>
2020-07-09 12:19:04 +0545

    minor improvements
    - purge css
    - bug fixes

--- postcss.config.js
+++ postcss.config.js
@@ -1,18 +1,34 @@
+const purgecss = require('@fullhuman/postcss-purgecss')
 const autoprefixer = require('autoprefixer')
 const cssnano = require('cssnano')
 const discardComments = require('postcss-discard-comments')
+
+const purgecssConfig = {
+  content: ['!(_site|node_modules)/**/*.+(html|js|md)', '*.html'],
+  whitelist: ['highlight', 'tooltip'],
+  whitelistPatterns: [/tooltip/, /bs-tooltip/],
+  whitelistPatternsChildren: [
+    /markdown-body/,
+    /highlight/,
+    /tooltip/,
+    /bs-tooltip/,
+  ],
+  defaultExtractor: (content) => content.match(/[\w-/:]+(?<!:)/g) || [],
+}
 
 module.exports = {
   plugins:
     process.env.NODE_ENV === 'production'
       ? [
           // NODE_ENV=production
+          purgecss(purgecssConfig),
           autoprefixer({ cascade: false }),
-          cssnano(),
+          cssnano({ preset: 'default' }),
           discardComments({ removeAll: true }),
         ]
       : [
           // NODE_ENV=development
+          purgecss(purgecssConfig),
           autoprefixer({ cascade: false }),
         ],
 }
